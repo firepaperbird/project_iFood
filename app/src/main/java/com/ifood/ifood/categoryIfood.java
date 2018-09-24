fd8a7e3a1cf7deecea30ef7df9d6b2977662f8ec
firepaperbird <thanvaquy1996@gmail.com>
2018-09-24 19:31:35 +0700

    1st commnit

---
+++ app/src/main/java/com/ifood/ifood/categoryIfood.java
@@ -0,0 +1,14 @@
+package com.ifood.ifood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class categoryIfood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_ifood);
+
+    }
+}
