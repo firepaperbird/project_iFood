a7d8152acfcc50655ce04f4a4002bf2993d801e7
firepaperbird <thanvaquy1996@gmail.com>
2018-10-04 18:35:24 +0700

    1st commnit

--- app/src/main/java/com/ifood/ifood/categoryIfood.java
+++ app/src/main/java/com/ifood/ifood/categoryIfood.java
@@ -2,6 +2,13 @@ package com.ifood.ifood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class categoryIfood extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class categoryIfood extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_ifood);
 
+        final LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+        final ViewTreeObserver observer= layout.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        setCateImgSize(layout.getHeight());
+                    }
+                });
+
+
+    }
+
+    private void setCateImgSize(int hight){
+        ImageView top = findViewById(R.id.topCate);
+        ImageView mid = findViewById(R.id.midCate);
+        ImageView bot = findViewById(R.id.botCate);
+        int categoryH = (hight-60)/3;
+        top.getLayoutParams().height = categoryH;
+        mid.getLayoutParams().height = categoryH;
+        bot.getLayoutParams().height = categoryH;
+    }
+
+    public void clickGym(View view) {
+
+    }
+
+    public void clickHealthy(View view) {
+    }
+
+    public void clickDaily(View view) {
     }
 }
--- app/src/main/java/com/ifood/ifood/wellcomeActivity.java
+++ app/src/main/java/com/ifood/ifood/wellcomeActivity.java
@@ -1,6 +1,8 @@
 package com.ifood.ifood;
 
 import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,7 +12,16 @@ public class wellcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome);
-//        ActionBar actionBar = getActionBar();
-//        actionBar.hide();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(wellcomeActivity.this, categoryIfood.class);
+                startActivity(intent);
+
+                finish(); //This closes current activity
+            }
+        }, 1700); //It means 4 seconds
     }
 }
