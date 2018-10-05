7c212e60302eb5021f8be50c641cdbaa7929027d
quanghuy323 <quanghuy323@users.noreply.github.com>
2018-10-05 13:23:20 +0700

    Commit layout main menu and footer layout

--- app/src/main/java/com/ifood/ifood/mainMenuActivity.java
+++ app/src/main/java/com/ifood/ifood/mainMenuActivity.java
@@ -1,6 +1,9 @@
 package com.ifood.ifood;
 
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -8,16 +11,70 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class mainMenuActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private LinearLayout listMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        setDrawerLayout();
+
+        LinearLayout.LayoutParams layoutMenu = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutMenu.setMargins(0,0,0,50);
+        LinearLayout.LayoutParams layoutParamsText = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        listMenu = (LinearLayout) findViewById(R.id.listMenu);
+
+        for (int i = 0; i < 5; i++){
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            layout.setLayoutParams(layoutMenu);
+
+            ImageView imageMenu = new ImageView(this);
+            imageMenu.setLayoutParams(new LinearLayout.LayoutParams(800, 400));
+            imageMenu.setImageResource(R.drawable.mon_ca_ri_ga);
+            imageMenu.setScaleType(ImageView.ScaleType.FIT_XY);
+
+            LinearLayout titleLayout = new LinearLayout(this);
+            titleLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView txtTitle = new TextView(this);
+            txtTitle.setLayoutParams(layoutParamsText);
+            txtTitle.setText("Cà ri gà");
+
+            Button btnViewDetail = new Button(this);
+            btnViewDetail.setBackgroundColor(Color.YELLOW);
+            btnViewDetail.setText("View Detail");
+            btnViewDetail.setLayoutParams(layoutParamsText);
+
+            titleLayout.addView(txtTitle);
+            titleLayout.addView(btnViewDetail);
+
+            TextView txtDescription = new TextView(this);
+            txtDescription.setLayoutParams(layoutParamsText);
+            txtDescription.setText("Cà ri gà công thức : 2 muỗng knor, 3 muỗng muối, 4 muỗng đường");
+
+            layout.addView(imageMenu);
+            layout.addView(titleLayout);
+            layout.addView(txtDescription);
+
+            listMenu.addView(layout);
+        }
         drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer);
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(drawerToggle);
@@ -61,5 +118,13 @@ public class mainMenuActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setDrawerLayout(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 }
