2f86d0f09deeb57aae797c8b66a7ca8c0fa375fa
quanghuy323 <quanghuy323@users.noreply.github.com>
2018-10-15 17:05:13 +0700

    Revert "Adding border for ingredient"
    This reverts commit b228f39

--- app/src/main/java/com/ifood/ifood/EditProfileActivity.java
+++ app/src/main/java/com/ifood/ifood/EditProfileActivity.java
@@ -3,6 +3,8 @@ package com.ifood.ifood;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.ifood.ifood.data.Model_User;
@@ -15,15 +17,33 @@ public class EditProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
 
         Intent intent = getIntent();
-        Model_User user = (Model_User)intent.getSerializableExtra("USERINFO");
+        final Model_User user = (Model_User)intent.getSerializableExtra("USERINFO");
 
-        EditText edtName = findViewById(R.id.editName);
+        final EditText edtName = findViewById(R.id.editName);
         edtName.setText(user.getUsername());
 
-        EditText edtAddress = findViewById(R.id.editAddress);
+        final EditText edtAddress = findViewById(R.id.editAddress);
         edtAddress.setText(user.getAddress());
 
-        EditText edtDescription = findViewById(R.id.editDescription);
+        final EditText edtDescription = findViewById(R.id.editDescription);
         edtDescription.setText(user.getDescription());
+
+        Button btnSave = findViewById(R.id.btnSaveUserInfo);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( EditProfileActivity.this, UserDetailActivity.class);
+
+                Model_User edtUser = new Model_User();
+                edtUser.setUsername(edtName.getText().toString());
+                edtUser.setAddress(edtAddress.getText().toString());
+                edtUser.setDescription(edtDescription.getText().toString());
+
+                intent.putExtra("USERINFO", edtUser);
+                startActivity(intent);
+
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/ifood/ifood/UserDetailActivity.java
+++ app/src/main/java/com/ifood/ifood/UserDetailActivity.java
@@ -16,16 +16,20 @@ public class UserDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail);
 
-        final Model_User user = new Model_User("Alibaba", "Alibaba@gmail.com", "1/23 Cong vien phan mem Quang Trung", "Tui la Alibaba");
+        Model_User user = (Model_User) getIntent().getSerializableExtra("USERINFO");
+        if (user == null) {
+            user = new Model_User("Alibaba", "Alibaba@gmail.com", "1/23 Cong vien phan mem Quang Trung", "Tui la Alibaba");
+        }
         TextView txtUsername = findViewById(R.id.txtUsername);
         txtUsername.setText(user.getUsername());
 
         Button editProfileBtn = findViewById(R.id.btnEditProfile);
+        final Model_User finalUser = user;
         editProfileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(UserDetailActivity.this, EditProfileActivity.class);
-                intent.putExtra("USERINFO", user);
+                intent.putExtra("USERINFO", finalUser);
                 startActivity(intent);
             }
         });
