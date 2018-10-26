e1cab943812c1bbafe35d5621b5faa3989dbaa39
quanghuy1103 <38184306+quanghuy1103@users.noreply.github.com>
2018-10-26 10:37:30 +0700

    update resize imgMain

--- app/src/main/java/com/ifood/ifood/TransactionConfirmActivity.java
+++ app/src/main/java/com/ifood/ifood/TransactionConfirmActivity.java
@@ -15,15 +15,29 @@ import com.ifood.ifood.data.Ingredient;
 import com.ifood.ifood.ultil.SessionLoginController;
 import com.ifood.ifood.ultil.SqliteShoppingListController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionConfirmActivity extends AppCompatActivity {
+
+//    Ingredient ingredient = new Ingredient(1, "Hanh cu", "1", "qua" );
+//    List<Ingredient> ingredientList = new ArrayList<Ingredient>();
+//    Dish dish = new Dish(1, "Mon ngon chua ngot ", " ngon", "huy", R.drawable.black_bean_bowl, null);
+//    List<Dish> dishList = new ArrayList<Dish>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_confirm);
+
+//        ingredientList.add(ingredient);
+//        dish.setIngredients(ingredientList);
+//        dishList.add(dish);
+
+        setContent();
     }
+
+
 
     private void setContent(){
         LinearLayout content = findViewById(R.id.detail);
@@ -42,10 +56,10 @@ public class TransactionConfirmActivity extends AppCompatActivity {
             txtDishName.setText(dish.getTitle());
 
             for (Ingredient ingredient: dish.getIngredients()) {
-                LinearLayout ingredientLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.layout_transaction_detail_dish, null);
+                LinearLayout ingredientLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.layout_transaction_detail_ingredient, null);
                 ingredientLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
-                EditText edtIngredientAmount = ingredientLayout.findViewWithTag("edtIngredientAmount");
+                TextView edtIngredientAmount = ingredientLayout.findViewWithTag("edtIngredientAmount");
                 edtIngredientAmount.setText(ingredient.getAmount());
                 edtIngredientAmount.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
                 edtIngredientAmount.setTag("IngredientAmount_" + dish.getId() + "_" + ingredient.getId());
